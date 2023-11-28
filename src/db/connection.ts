76d7326225c82ac21feb8fc84e7334b854faff31
Willinton Mora <wifermen@hotmail.com>
2023-11-28 15:38:49 -0500

    feat: update database connection configuration

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,9 +1,11 @@
 import { Sequelize } from 'sequelize';
 
 
-const host = process.env.DB_HOST;
+const host = process.env.DB_HOST || 'localhost';
+const user = process.env.DB_USER || 'root';
+const password = process.env.DB_PASSWORD;
 
-const db = new Sequelize('gymdb', 'root','123456', {
+const db = new Sequelize('gymdb', user, password, {
     host,
     dialect: 'mysql',
     port: 3306,
